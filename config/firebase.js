9f0fb704779828852f7229c4863140f13cd25309
Van Ba Phat Huy <phathuy2004h@gmail.com>
2026-04-21 22:13:18 +0700

    21/04/2026

--- config/firebase.js
+++ config/firebase.js
@@ -1,32 +1,32 @@
-// const admin = require('firebase-admin');
-// const serviceAccount = require("../config/personalfinancetracker-ec7dd-firebase-adminsdk-fbsvc-c9efd07e18.json");
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-// });
-
-// const db = admin.firestore();
-// const auth = admin.auth();
-
-// module.exports = { db, auth, admin };
-
 const admin = require('firebase-admin');
+const serviceAccount = require("../config/personalfinancetracker-ec7dd-firebase-adminsdk-fbsvc-c9efd07e18.json");
 
-// Lấy chuỗi JSON từ biến môi trường mà Huy vừa dán lên Vercel
-const serviceAccountRaw = process.env.FIREBASE_SERVICE_ACCOUNT;
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
-if (serviceAccountRaw) {
-    try {
-        // Parse cái chuỗi đó thành Object
-        const serviceAccount = JSON.parse(serviceAccountRaw);
+const db = admin.firestore();
+const auth = admin.auth();
 
-        if (!admin.apps.length) {
-            admin.initializeApp({
-                credential: admin.credential.cert(serviceAccount)
-            });
-            console.log("Firebase Admin đã kết nối thành công!");
-        }
-    } catch (error) {
-        console.error("Lỗi khi parse JSON Firebase:", error);
-    }
-}
+module.exports = { db, auth, admin };
+
+// const admin = require('firebase-admin');
+
+// // Lấy chuỗi JSON từ biến môi trường mà Huy vừa dán lên Vercel
+// const serviceAccountRaw = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+// if (serviceAccountRaw) {
+//     try {
+//         // Parse cái chuỗi đó thành Object
+//         const serviceAccount = JSON.parse(serviceAccountRaw);
+
+//         if (!admin.apps.length) {
+//             admin.initializeApp({
+//                 credential: admin.credential.cert(serviceAccount)
+//             });
+//             console.log("Firebase Admin đã kết nối thành công!");
+//         }
+//     } catch (error) {
+//         console.error("Lỗi khi parse JSON Firebase:", error);
+//     }
+// }
